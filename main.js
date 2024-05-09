aa381ffbe4e3639ab59b0f72b903d495605a4a41
emahji2683 <165803322+emahji2683@users.noreply.github.com>
2024-05-09 22:32:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,24 @@
 {
   const btn = document.getElementById('btn');
   const result = document.getElementById('result')
+  const resultsub = document.getElementById('resultsub')
   btn.addEventListener('click', () => {
     const n = Math.floor(Math.random() * 3);
 
     switch (n) {
       case 0:
-        result.textContent = '大吉(景品：甘い物)';
+        result.textContent = '大吉';
+        resultsub.textContent = 'なんと甘いものをおごります';
         break;
 
       case 1:
         result.textContent = '中吉';
+        resultsub.textContent = '';
         break;
 
       case 2:
         result.textContent = '小吉';
+        resultsub.textContent = '';
         break;
     }
   })
